5d3f1424b39f914e20a0f11abfa3eb10dd72b75e
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-02-24 15:04:13 +0000

    task completed

--- src/App.js
+++ src/App.js
@@ -1,54 +1,91 @@
 import "./App.css"
+import {useState} from 'react'
 
 function App() { 
 
-  return (
+  const [firstNumber, setFirstNumber] = useState('0')
+  const [secondNumber, setSecondNumber] = useState('0')
+  const [operation, setOperation] = useState('+')
+  const [store, setStore] = useState(0)
+
+    let total = 0
+
+    const firstNumberButton = (numberAsString) => {
+      firstNumber === '0' ? setFirstNumber(numberAsString) : setFirstNumber(firstNumber + numberAsString)
+    }
+
+    const secondNumberButton = (numberAsString) => {
+      secondNumber === '0' ? setSecondNumber(numberAsString) : setSecondNumber(secondNumber + numberAsString)
+    }
+
+    if(operation === '+') {
+      total = Number(firstNumber) + Number(secondNumber)
+    }
+
+    if(operation === '-') {
+      total = Number(firstNumber) - Number(secondNumber)
+    }
+
+    if(operation === '*') {
+      total = Number(firstNumber) * Number(secondNumber)
+    }
+
+    if(operation === '÷') {
+      total = Number(firstNumber) / Number(secondNumber)
+    }
+
+    return (
     <div className="calculator">
         <div className="panel">
-          <p>0</p>
+          <p>{firstNumber}</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+            <button onClick={() => firstNumberButton('1')}>1</button>
+            <button onClick={() => firstNumberButton('2')}>2</button>
+            <button onClick={() => firstNumberButton('3')}>3</button>
+            <button onClick={() => firstNumberButton('4')}>4</button>
+            <button onClick={() => firstNumberButton('5')}>5</button>
+            <button onClick={() => firstNumberButton('6')}>6</button>
+            <button onClick={() => firstNumberButton('7')}>7</button>
+            <button onClick={() => firstNumberButton('8')}>8</button>
+            <button onClick={() => firstNumberButton('9')}>9</button>
+            <button onClick={() => firstNumberButton('0')}>0</button>
+            <button onClick={() => setFirstNumber('0')}>Clear</button>
+            <button onClick={() => setFirstNumber(store.toString())}>Recall</button>
+            <button onClick={() => firstNumber.includes('.') ? setFirstNumber(firstNumber) : setFirstNumber(firstNumber + '.')}>.</button>
           </div>
         </div>
         
         <div className="panel">
-          <p>+</p>
+          <p>{operation}</p>
           <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
+            <button onClick={() => setOperation('+')}>+</button>
+            <button onClick={() => setOperation('-')}>-</button>
+            <button onClick={() => setOperation('*')}>*</button>
+            <button onClick={() => setOperation('÷')}>÷</button>
           </div>
         </div>
 
         <div className="panel">
-          <p>0</p>
+          <p>{secondNumber}</p>
           <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
+            <button onClick={() => secondNumberButton('1')}>1</button>
+            <button onClick={() => secondNumberButton('2')}>2</button>
+            <button onClick={() => secondNumberButton('3')}>3</button>
+            <button onClick={() => secondNumberButton('4')}>4</button>
+            <button onClick={() => secondNumberButton('5')}>5</button>
+            <button onClick={() => secondNumberButton('6')}>6</button>
+            <button onClick={() => secondNumberButton('7')}>7</button>
+            <button onClick={() => secondNumberButton('8')}>8</button>
+            <button onClick={() => secondNumberButton('9')}>9</button>
+            <button onClick={() => secondNumberButton('0')}>0</button>
+            <button onClick={() => setSecondNumber('0')}>Clear</button>
+            <button onClick={() => setSecondNumber(store.toString())}>Recall</button>
+            <button onClick={() => secondNumber.includes('.') ? setSecondNumber(secondNumber) : setSecondNumber(secondNumber + '.')}>.</button>
           </div>
         </div>
         <div className="panel answer">
-          <p>0</p>
+          <p>{total}</p>
+          <button onClick={() => setStore(total)}>Store</button>
         </div>
     </div>
   )
